86ba5bdec42fd8958e8be4068270ff8682bd6755
Jocelyn Delalande <jocelyn@delalande.fr>
2011-08-27 11:30:21 +0000

    fixes #23, #24 .included patch for mysql, avoiding errno 121 and syntax errors from Bill Stifler, thanks !

--- SQL/mysql.initial.sql
+++ SQL/mysql.initial.sql
@@ -1,18 +1,14 @@
 CREATE TABLE `collected_contacts` (
- `contact_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
- `changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
- `del` tinyint(1) NOT NULL DEFAULT '0',
- `name` varchar(128) NOT NULL,
- `email` varchar(128) NOT NULL,
- `firstname` varchar(128) NOT NULL,
- `surname` varchar(128) NOT NULL,
- `vcard` text NULL,
- `user_id` int(10) UNSIGNED NOT NULL DEFAULT '0',
- PRIMARY KEY(`contact_id`),
- INDEX `user_collected_contacts_index` (`user_id`,`email`),
- CONSTRAINT `user_id_fk_contacts` FOREIGN KEY (`user_id`)
-   REFERENCES `users`(`user_id`)
-   --!40008
-   --ON DELETE CASCADE
-   --ON UPDATE CASCADE
-) ;
+`contact_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+`changed` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+`del` tinyint(1) NOT NULL DEFAULT '0',
+`name` varchar(128) NOT NULL,
+`email` varchar(128) NOT NULL,
+`firstname` varchar(128) NOT NULL,
+`surname` varchar(128) NOT NULL,
+`vcard` text,
+`user_id` int(10) unsigned NOT NULL DEFAULT '0',
+PRIMARY KEY (`contact_id`),
+KEY `user_collected_contacts_index` (`user_id`,`email`),
+CONSTRAINT `collected_contacts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
