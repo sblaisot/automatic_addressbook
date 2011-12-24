3a7769c4ad6a2229317d8b11c4c8e47338b896e0
Jocelyn Delalande <jocelyn@delalande.fr>
2011-12-24 16:53:46 +0000

    Reverted to manual table creation with sqlite instead of copying via a SELECT… Cause things like PRIMARY KEY weren't preserved.

--- SQL/sqlite.update.sql
+++ SQL/sqlite.update.sql
@@ -1,7 +1,18 @@
 CREATE TABLE collected_contacts_tmp AS SELECT * FROM collected_contacts;
 DROP TABLE collected_contacts;
 
-CREATE TABLE collected_contacts AS SELECT * FROM contacts WHERE 1=0;
+CREATE TABLE collected_contacts (
+    contact_id integer NOT NULL PRIMARY KEY,
+    user_id integer NOT NULL default '0',
+    changed datetime NOT NULL default '0000-00-00 00:00:00',
+    del tinyint NOT NULL default '0',
+    name varchar(128) NOT NULL default '',
+    email varchar(255) NOT NULL default '',
+    firstname varchar(128) NOT NULL default '',
+    surname varchar(128) NOT NULL default '',
+    vcard text NOT NULL default '',
+    words text NOT NULL default ''
+);
 
 INSERT INTO collected_contacts (contact_id, user_id, changed, del, name, email, firstname, surname, vcard)
     SELECT contact_id, user_id, changed, del, name, email, firstname, surname, vcard FROM collected_contacts_tmp;
