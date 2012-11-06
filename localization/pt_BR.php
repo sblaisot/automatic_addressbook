4c7e2f729e28006f463a423cd4387b6b8b78d5aa
Sebastien Blaisot <sebastien@blaisot.org>
2012-11-06 06:59:42 +0000

    Added Portuguese Brazilian translation. Thanks to Denis Volpato Martins

---
+++ localization/pt_BR.php
@@ -0,0 +1,7 @@
+<?php
+$labels = array();
+$labels['automaticallycollected']  = 'Coletado Automaticamente';
+$labels['useautoabook']  = 'Utilizar catálogo de endereços automático';
+$labels['useforcompletion'] = 'Utilizar endereços coletados automaticamente para autocompletar';
+$labels['contactremoved'] = 'Contato(s) apagados do catálogo de endereços automático';
+$labels['contactmoved'] = 'Contato(s) movidos do catálogo de endereços automático';
